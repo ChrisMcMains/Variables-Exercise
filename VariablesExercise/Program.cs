1713e04efb0f79361c11536e73ee8bf1270c0151
chris mcmains <mcmainsc84@gmail.com>
2023-05-26 19:04:59 -0400

    Variable Exercise Completed

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿using System;
+using System.Net.Cache;
+
+string name = "Chris";
+int age = 38;
+char middleInitial = 'S';
+bool isMarried = true;
+double currentTemp = 78.4;
+decimal currentPrice = 3.79m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Married?: {isMarried}");
+Console.WriteLine($"Temp: {currentTemp}");
+Console.WriteLine($"Price: {currentPrice}");
+
+Console.WriteLine($"My name is {name} and I am {age} years old.  The current temperature is {currentTemp} and gas cost {currentPrice}");
